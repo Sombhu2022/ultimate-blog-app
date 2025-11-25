51d1a37c323733766d8c2263986628302c95b64f
Sombhu das <105740734+Sombhu2022@users.noreply.github.com>
2025-11-25 21:06:36 +0530

    Add files via upload

--- src/services/blog.service.js
+++ src/services/blog.service.js
@@ -23,3 +23,65 @@ export const getAllBlogsService = async()=>{
     }
 
 }
+
+export const updateBlogData = async(id , body )=>{
+  
+    try {
+        
+        const res = await Blog.findByIdAndUpdate({ _id : id } ,{ $set :{ title : body.title , description : body.description }} , { new : true})
+        return res
+
+    } catch (error) {
+        return error
+    }
+
+}
+
+
+export const updateLikes = async(blogId , userId)=>{
+ 
+    try {
+        const res = await Blog.findById(blogId)
+        if(!res){
+            throw new Error (" Blog not found !")
+        }
+        const isLiked = res.like.includes(userId);
+        if(isLiked){
+            //unlike
+        const filterLikes = res.like.filter(item => item !== userId) // all likes id without userId
+        res.like = filterLikes
+        await res.save()
+        return res
+
+        }else{
+            // like
+            res.like.push(userId)
+            // res.like = [ ...res.like , userId ]
+            await res.save()
+            return res
+
+        }
+
+
+    } catch (error) {
+
+        return error
+        
+    }
+
+}
+
+export const deleteBlogById = async(id)=>{
+    try {
+
+        const isExist = await Blog.findById(id)
+        if(!isExist){
+            throw new Error (" Blog not found !")
+        }
+
+        await Blog.findByIdAndDelete(id)
+        
+    } catch (error) {
+        
+    }
+}
